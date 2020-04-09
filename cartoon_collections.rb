a9af72433c27dd9ccbe5b8f46daf8cbe9e590b99
Klarissa Smith <ksmith_email@yahoo.com>
2020-04-09 15:08:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,10 @@
-# def greet_characters(array)
-#   # Use `each` to enumerate over the provided array
-#   #
-#   # Print a custom greeting for each element
-#   array.each do |array|
-#     puts "#{array} isn't sure if this should be unique for each element"
-# end
+def greet_characters(array)
+  # Use `each` to enumerate over the provided array
+  #
+  # Print a custom greeting for each element
+  array.each do |array|
+    puts "#{array} isn't sure if this should be unique for each element"
+end
 
 # def list_dwarves(array)
 #   # Use `each_with_index` to enumerate over the provided array
