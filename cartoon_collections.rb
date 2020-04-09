eeecd5cf9beb7b907b062dd80389b3f17337a6ca
Klarissa Smith <ksmith_email@yahoo.com>
2020-04-09 15:12:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |array|
-    puts "#{array} isn't sure if this should be unique for each element"
+    puts "#{array[0]} isn't sure if this should be unique for each element"
   end
 end
 
