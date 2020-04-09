dbdf0b4392236797e7e70edff4a6fc652af3fd9f
Klarissa Smith <ksmith_email@yahoo.com>
2020-04-09 15:02:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  array.each do |array|
-    puts "#{array.capitalize} isn't sure if this should be unique for each element"
+  array.each do |array[i]|
+    puts "#{array} isn't sure if this should be unique for each element"
 end
 
 def list_dwarves(array)
